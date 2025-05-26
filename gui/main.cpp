89d9b81e7aa21ba01992dfeb19b1f75bbc74f68a
Valentin22r <valentin.roquejofre@epitech.eu>
2025-05-26 16:02:07 +0200

    Add a window that open a window and close it after 3 seconds

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
-auto main() -> int { std::cout << "Hello, client!\n"; }
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0 ) {
+        fprintf(stdout,"Error :(%s)\n",SDL_GetError());
+        return -1;
+    }
+    {
+        SDL_Window* pWindow = NULL;
+        pWindow = SDL_CreateWindow("Window",SDL_WINDOWPOS_UNDEFINED,
+                                                                  SDL_WINDOWPOS_UNDEFINED,
+                                                                  640,
+                                                                  480,
+                                                                  SDL_WINDOW_SHOWN);
+
+        if( pWindow ) {
+            SDL_Delay(3000);
+
+            SDL_DestroyWindow(pWindow);
+        } else {
+            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+        }
+    }
+    SDL_Quit();
+
+    return 0;
+}
