fb481d02c5d485d7c001ac990966aea2ab087821
Sigmanificient <edhyjox@gmail.com>
2025-05-27 19:02:03 +0200

    Fix pre commit formatter to use cpp-fmt

--- nix/pre-commit-hooks.nix
+++ nix/pre-commit-hooks.nix
@@ -1,7 +1,6 @@
 {
   self,
   pkgs,
-  pre-commit-hooks,
 }: let
   inherit (pkgs) lib;
 
@@ -11,42 +10,34 @@
   align-slashes = python-script "align-slashes" ../scripts/align_columns.py;
 
   discard-headers = python-script "discard-headers" ../scripts/discard_headers.py;
-
-  hooks = {
-    alejandra.enable = true;
-    clang-format = {
-      enable = true;
-      types_or = lib.mkForce [
-        "c++"
-      ];
-    };
-
-    align-slashes = {
-      enable = true;
-      name = "align blackslashes";
-      entry = lib.getExe align-slashes;
-    };
-
-    discard-headers = {
-      enable = true;
-      name = "discard headers";
-      entry = lib.getExe discard-headers;
-    };
-
-    trim-trailing-whitespace.enable = true;
-
-    commit-name = {
-      enable = true;
-      name = "commit name";
-      stages = ["commit-msg"];
-      entry = ''
-        ${pkgs.python310.interpreter} ${../scripts/check_commit_message.py}
-      '';
-    };
-  };
 in {
-  pre-commit-check = pre-commit-hooks.lib.${pkgs.system}.run {
-    inherit hooks;
-    src = ./.;
+  alejandra.enable = true;
+  clang-format = {
+    enable = true;
+    name = "format the code";
+    entry = lib.getExe self.packages.${pkgs.system}.cpp-fmt;
+  };
+
+  align-slashes = {
+    enable = true;
+    name = "align blackslashes";
+    entry = lib.getExe align-slashes;
+  };
+
+  discard-headers = {
+    enable = true;
+    name = "discard headers";
+    entry = lib.getExe discard-headers;
+  };
+
+  trim-trailing-whitespace.enable = true;
+
+  commit-name = {
+    enable = true;
+    name = "commit name";
+    stages = ["commit-msg"];
+    entry = ''
+      ${pkgs.python310.interpreter} ${../scripts/check_commit_message.py}
+    '';
   };
 }
