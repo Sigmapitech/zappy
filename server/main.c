b507a07905e17c2c041d9a9b1897a1e8bdf85e69
Ciznia <gabriel.hosquet@epitech.eu>
2025-05-27 11:57:56 +0200

    Add arg parser based on getopt-long

--- server/main.c
+++ server/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "args_parser.h"
+
+/**
+ * @brief Program entry point.
+ * @param argc size of argv
+ * @param argv command line arguments
+ * @return int Program exit status
+ */
 [[gnu::weak]]
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("Hello, server!\n");
+    params_t params = {0};
+
+    if (!parse_args(&params, argc, argv))
+        return 84;
+    if (params.help)
+        return printf("%s\n", USAGE), free((void *)params.teams), EXIT_SUCCESS;
+    free((void *)params.teams);
+    return EXIT_SUCCESS;
 }
