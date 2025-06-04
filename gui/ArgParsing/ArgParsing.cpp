8b1191eff7439bcbe1da2f2a4e43949eea46e531
Valentin22r <valentin.roquejofre@epitech.eu>
2025-06-04 19:05:11 +0200

    Add a Parsing class

---
+++ gui/ArgParsing/ArgParsing.cpp
@@ -0,0 +1,53 @@
+#include "ArgParsing.hpp"
+#include <string>
+
+ArgParsing::ArgParsing(int port_arg, std::string &hostname_arg)
+  : _port(port_arg), _hostname(hostname_arg)
+{
+}
+
+namespace {
+  constexpr unsigned int hash(const char *str)
+  {
+    unsigned int h = 5381;
+    for (; *str != '\0'; str++)
+      h = (h * 33) ^ (unsigned char)*str;
+    return h;
+  }
+}  // namespace
+
+bool ArgParsing::ParseArg(int ac, char **av)
+{
+  if (ac == 0)
+    return false;
+
+  for (int i = 1; i < ac; i++) {
+    switch (hash(av[i])) {
+
+      case hash("-h"):
+      case hash("--hostname"):
+        if (i == ac - 1)
+          return false;
+        _hostname = av[i + 1];
+        i++;
+        break;
+
+      case hash("-p"):
+      case hash("--port"):
+        if (i == ac - 1)
+          return false;
+        _port = std::stoi(av[i + 1]);
+        i++;
+        break;
+
+      case hash("-help"):
+      case hash("--help"):
+        _help = true;
+        return true;
+
+      default:
+        return false;
+    }
+  }
+  return true;
+}
