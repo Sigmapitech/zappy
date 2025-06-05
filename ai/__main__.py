579af0f39c4be0b28b8426376b1040b6decb7dc2
Sigmanificient <edhyjox@gmail.com>
2025-06-05 23:29:59 +0200

    Populate the argparser using a dict descriptor

--- ai/__main__.py
+++ ai/__main__.py
@@ -1,43 +1,48 @@
 import argparse
-import signal
-import sys
 from contextlib import contextmanager
+from typing import Any, Sequence
 
 from .player import Player
 
+__version__ = "0.0.1"
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(
-        description="Player for the game", add_help=False
-    )
-    parser.add_argument(
-        "-p",
-        "--port",
+CLI_ARGS: dict[Sequence[str], dict[str, Any]] = {
+    ("-p", "--port"): dict(
         type=int,
         required=True,
         help="The port of the server (e.g., 4242)",
-    )
-    parser.add_argument(
-        "-n",
-        "--name",
+    ),
+    ("-n", "--name"): dict(
         type=str,
         required=True,
         help='The name of the team (e.g., "toto")',
-    )
-    parser.add_argument(
-        "-h",
-        "--host",
+    ),
+    ("-h", "--host"): dict(
         type=str,
         required=True,
         help='The address of the server (e.g., "localhost")',
-    )
-    parser.add_argument(
-        "-a",
-        "--help",
+    ),
+    ("-a", "--help"): dict(
         action="help",
         default=argparse.SUPPRESS,
         help="Show this help message and exit",
+    ),
+    "--version": dict(
+        action="version", version=f"{__package__}, v{__version__}"
+    ),
+}
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Player for the game", add_help=False
     )
+
+    for flags, settings in CLI_ARGS.items():
+        if isinstance(flags, str):
+            flags = (flags,)
+
+        parser.add_argument(*flags, **settings)
     return parser.parse_args()
 
 
@@ -51,11 +56,13 @@ def game(player):
 
 def main():
     # signal.signal(signal.SIGINT, lambda *_: sys.exit(0))
-    args = parse_arguments()
+    args = parse_args()
+
     print("args:", args)
     server_address = (args.host, args.port)
     team_name = args.name
-    with game(player := Player(server_address, team_name)) as player:
+
+    with game(Player(server_address, team_name)) as player:
         player.main_loop()
 
 
