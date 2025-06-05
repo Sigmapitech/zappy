e81b2fce6698e2756a69190945f701a259c56b0b
Sigmanificient <edhyjox@gmail.com>
2025-06-06 00:15:42 +0200

    Reformat the python files

--- docs/conf.py
+++ docs/conf.py
@@ -1,33 +1,33 @@
 import os
-import sys
 import pathlib
+import sys
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath("../"))
 
-project = 'ZAPPY'
-author = 'Team Hermitcraft'
-release = '1.0'
+project = "ZAPPY"
+author = "Team Hermitcraft"
+release = "1.0"
 extensions = [
-    'breathe',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "breathe",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
     "myst_parser",
     "exhale",
 ]
-html_theme = 'furo'
+html_theme = "furo"
 
 ZAPPY_ROOT = pathlib.Path(__file__).parent.parent
 breathe_projects = {"Zappy": f"{ZAPPY_ROOT}/.build/doxygen/xml"}
 breathe_default_project = "Zappy"
 
 exclude_patterns = []
-templates_path = ['_templates']
-html_static_path = ['_static']
-html_extra_path = ['assets']
+templates_path = ["_templates"]
+html_static_path = ["_static"]
+html_extra_path = ["assets"]
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
 
 exhale_args = {
