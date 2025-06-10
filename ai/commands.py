b876d2d684ccce03b7da38c082bf1db2f22f6543
hop3calypse <hugo.hamet@proton.me>
2025-06-10 14:17:50 +0200

    Change command sending to private

--- ai/commands.py
+++ ai/commands.py
@@ -10,7 +10,7 @@ class Commands:
         self.network = network
         self.team_name = team_name
 
-    def send_command(self, command: str) -> str:
+    def __send_command(self, command: str) -> str:
         print(f"Sending: {command}")
         self.network.send_message(command)
         response = self.network.receive_message()
@@ -18,32 +18,32 @@ class Commands:
         return response
 
     def look(self) -> str:
-        return self.send_command("Look")
+        return self.__send_command("Look")
 
     def take(self, obj: str) -> str:
-        return self.send_command(f"Take {obj}")
+        return self.__send_command(f"Take {obj}")
 
     def set(self, obj: str) -> str:
-        return self.send_command(f"Set {obj}")
+        return self.__send_command(f"Set {obj}")
 
     def move_up(self) -> str:
-        return self.send_command("Forward")
+        return self.__send_command("Forward")
 
     def turn_right(self) -> str:
-        return self.send_command("Right")
+        return self.__send_command("Right")
 
     def turn_left(self) -> str:
-        return self.send_command("Left")
+        return self.__send_command("Left")
 
     def start_incantation(self) -> str:
-        return self.send_command("Incantation")
+        return self.__send_command("Incantation")
 
     def broadcast(self, message: str) -> str:
         self.network.send_message(f"Broadcast {message}")
         return self.network.receive_message()
 
     def fork(self):
-        res = self.send_command("Fork")
+        res = self.__send_command("Fork")
 
         if res == "ko":
             return res
@@ -55,10 +55,10 @@ class Commands:
         return res
 
     def connect_nbr(self) -> str:
-        return self.send_command("Connect_nbr")
+        return self.__send_command("Connect_nbr")
 
     def inventory(self) -> str:
-        return self.send_command("Inventory")
+        return self.__send_command("Inventory")
 
     def eject(self) -> str:
-        return self.send_command("Eject")
+        return self.__send_command("Eject")
