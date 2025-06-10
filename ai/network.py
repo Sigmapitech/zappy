5ac7b31484eda3106014d9025bbc3cb2ab64b621
hop3calypse <hugo.hamet@proton.me>
2025-06-10 18:14:42 +0200

    Fix logging messages

--- ai/network.py
+++ ai/network.py
@@ -2,6 +2,18 @@ import logging
 import socket
 import time
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)-11s - %(levelname)-8s - %(message)s",
+    datefmt="%H:%M:%S",
+)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(stream_handler)
+
 
 class Network:
     def __init__(
@@ -20,7 +32,7 @@ class Network:
                 self.sock.connect(self.server_address)
                 return
             except socket.error as e:
-                logging.warning(
+                logger.warning(
                     f"Connection failed: {e}. Retrying in {self.delay} seconds..."
                 )
                 time.sleep(self.delay)
@@ -29,10 +41,10 @@ class Network:
         )
 
     def send_message(self, message: str):
-        self.sock.sendall(f"{message}\n".encode("utf-8"))
+        self.sock.sendall(f"{message}\n".encode("ascii"))
 
     def receive_message(self) -> str:
-        return self.sock.recv(self.buffer_size).decode("utf-8").strip()
+        return self.sock.recv(self.buffer_size).decode("ascii").strip()
 
     def close(self):
         if self.sock:
