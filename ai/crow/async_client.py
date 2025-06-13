59f731b302051b96b1ff8ca7e24c0564b6d40a03
Sigmanificient <edhyjox@gmail.com>
2025-06-13 11:29:03 +0200

    Implement a start lib for the async client

---
+++ ai/crow/async_client.py
@@ -0,0 +1,79 @@
+import asyncio
+import socket
+
+
+class AsyncSocketClient:
+
+    def __init__(self, host, port, broadcast_callback):
+        self._host = host
+        self._port = port
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.setblocking(False)
+        self._loop = asyncio.get_event_loop()
+
+        self._recv_task = None
+        self._incoming_queue = asyncio.Queue()
+        self.command_lock = asyncio.Lock()
+
+        self._connected = False
+        self._broadcast_callback = broadcast_callback
+
+    async def connect(self):
+        await self._loop.sock_connect(self._sock, (self._host, self._port))
+        self._connected = True
+        self._recv_task = asyncio.create_task(self._receiver_loop())
+
+    async def send_command(self, command: str) -> str:
+        async with self.command_lock:
+            if not self._connected:
+                raise ConnectionError("Not connected")
+            await self._send_line(command)
+            return await self._next_non_message()
+
+    async def _next_non_message(self) -> str:
+        while True:
+            msg = await self._incoming_queue.get()
+
+            if msg.startswith("message "):
+                # todo: parse the broadcast
+                await self._broadcast_callback(0, f">>> {msg}")
+                continue
+
+            if msg == "dead":
+                await self.close()
+                raise ConnectionError("Client died.")
+
+            return msg
+
+    async def _wait_for_exact(self, expected: str):
+        while True:
+            msg = await self._next_non_message()
+            if msg == expected:
+                return
+            raise ValueError(f"Unexpected message: {msg}")
+
+    async def _send_line(self, line: str):
+        await self._loop.sock_sendall(self._sock, (line + "\n").encode())
+
+    async def _receiver_loop(self):
+        buffer = b""
+        try:
+            while self._connected:
+                data = await self._loop.sock_recv(self._sock, 4096)
+                if not data:
+                    break
+                buffer += data
+                while b"\n" in buffer:
+                    line, buffer = buffer.split(b"\n", 1)
+                    message = line.decode().strip()
+                    await self._incoming_queue.put(message)
+        finally:
+            self._connected = False
+            await self._incoming_queue.put("dead")
+
+    async def close(self):
+        self._connected = False
+        if self._recv_task:
+            self._recv_task.cancel()
+
+        self._sock.close()
---
+++ ai/crow/command_manager.py
@@ -0,0 +1,55 @@
+import os
+import sys
+
+from .async_client import AsyncSocketClient
+
+
+class CommandManager:
+
+    def __init__(self, sock: AsyncSocketClient):
+        self._sock = sock
+
+    async def look(self) -> str:
+        return await self._sock.send_command("Look")
+
+    async def take(self, obj: str) -> str:
+        return await self._sock.send_command(f"Take {obj}")
+
+    async def set(self, obj: str) -> str:
+        return await self._sock.send_command(f"Set {obj}")
+
+    async def move_up(self) -> str:
+        return await self._sock.send_command("Forward")
+
+    async def turn_right(self) -> str:
+        return await self._sock.send_command("Right")
+
+    async def turn_left(self) -> str:
+        return await self._sock.send_command("Left")
+
+    async def start_incantation(self) -> str:
+        return await self._sock.send_command("Incantation")
+
+    async def broadcast(self, message: str):
+        return await self._sock.send_command(f'Broadcast "{message}"')
+
+    async def fork(self):
+        res = await self._sock.send_command("Fork")
+
+        if res == "ko":
+            return res
+
+        pid = os.fork()
+        if pid == 0:
+            os.execv(sys.executable, [sys.executable] + sys.argv)
+
+        return res
+
+    async def connect_nbr(self) -> str:
+        return await self._sock.send_command("Connect_nbr")
+
+    async def inventory(self) -> str:
+        return await self._sock.send_command("Inventory")
+
+    async def eject(self) -> str:
+        return await self._sock.send_command("Eject")
