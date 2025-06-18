9d426edbf6d34df945e7d40497e5cdd89ea8a4b1
ElectronicIV <louis.persin@epitech.eu>
2025-06-18 16:00:07 +0200

    Add multi mesh object support

---
+++ gui/Renderer/Object3D.hpp
@@ -0,0 +1,17 @@
+#include <memory>
+#include <unordered_map>
+
+#include "Mesh.hpp"
+
+class Object3D {
+private:
+  std::unordered_map<std::string, size_t> _idMap;
+
+public:
+  glm::mat4 modelMatrix = glm::mat4(1.0);  // NOLINT
+
+  std::vector<std::unique_ptr<Mesh>> _meshArr;
+
+  Object3D(const std::string &path);
+  void Draw(GLuint shader, const glm::mat4 &view, const glm::mat4 &proj) const;
+};
