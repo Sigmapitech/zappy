1e4be69cde75e4176a862acd26dffea892e59c91
Valentin22r <valentin.roquejofre@epitech.eu>
2025-06-18 14:35:29 +0200

    Add Api for the gui client

--- gui/API/API.hpp
+++ gui/API/API.hpp
@@ -4,19 +4,47 @@
 #include "API/Trantor/Trantor.hpp"
 
 #include <map>
+#include <mutex>
 #include <string>
 #include <vector>
+
+class Incantation {
+private:
+  std::vector<int> _member;
+  int _level;
+  std::pair<int, int> _position;
+  std::string _state;
+
+public:
+  Incantation(int level, int x, int y);
+  ~Incantation() = default;
+
+  void AddMember(int id);
+  std::pair<int, int> GetPosition();
+  void SetState(std::string &state);
+};
 
 class API {
 private:
   std::vector<std::string> _command;
-  Tilemap _map;
+  std::mutex _commandListLocker;
+
+  Tilemap _tilemap;
   std::vector<std::string> _allTeamName;
   std::map<std::string, std::vector<Trantor>> _teams;
+  std::vector<Incantation> _incantationList;
+  std::map<int, std::pair<int, int>> _eggList;
+  short _timeUnit = 0;
+  bool _isGameRunning = false;
+  std::string _winner;
+  std::vector<std::string> _serverMessage;
 
 public:
   API() = default;
   ~API() = default;
+
+  void AddEgg(int id, int x, int y);
+  void DeleteEgg(int id);
 
   // FROM GIU
   void AskMapSize();
@@ -44,4 +72,29 @@ public:
     int orientation,
     int level,
     std::string teamsName);
+  void HandleMSZ(std::stringstream &ss);
+  void HandleBCT(std::stringstream &ss);
+  static void HandleMCT();
+  void HandleTNA(std::stringstream &ss);
+  void HandlePNW(std::stringstream &ss);
+  void HandlePPO(std::stringstream &ss);
+  void HandlePLV(std::stringstream &ss);
+  void HandlePIN(std::stringstream &ss);
+  void HandlePEX(std::stringstream &ss);
+  void HandlePBC(std::stringstream &ss);
+  void HandlePIC(std::stringstream &ss);
+  void HandlePIE(std::stringstream &ss);
+  void HandlePFK(std::stringstream &ss);
+  void HandlePDR(std::stringstream &ss);
+  void HandlePGT(std::stringstream &ss);
+  void HandlePDI(std::stringstream &ss);
+  void HandleENW(std::stringstream &ss);
+  void HandleEBO(std::stringstream &ss);
+  void HandleEDI(std::stringstream &ss);
+  void HandleSGT(std::stringstream &ss);
+  void HandleSST(std::stringstream &ss);
+  void HandleSEG(std::stringstream &ss);
+  void HandleSMG(std::stringstream &ss);
+  static void HandleSUC();
+  static void HandleSBP();
 };
--- gui/API/Trantor/Trantor.hpp
+++ gui/API/Trantor/Trantor.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include "API/Inventory/Inventory.hpp"
 
 class Trantor {
@@ -7,11 +8,34 @@ private:
   Inventory _inventory;
   int _food;
   int _id;
+  int _orientation;
+  int _level;
   std::pair<int, int> _position;  // First int = x, Second int = y
+  std::vector<std::string> _broadcast;
+  bool _isLaying;
 
 public:
   Trantor() = default;
+  Trantor(int id, int x, int y, int orientation, int level);
   ~Trantor() = default;
 
   [[nodiscard]] int GetId() const;
+
+  void SetPosition(int x, int y, int orientation);
+  void SetPosition(int x, int y);
+  std::pair<int, int> GetPosition();
+
+  void SetLevel(int level);
+  void SetInventory(
+    size_t q0,
+    size_t q1,
+    size_t q2,
+    size_t q3,
+    size_t q4,
+    size_t q5,
+    size_t q6);
+  void AddToInventory(Item item, int quantity);
+  void SetBroadcast(std::string &message);
+
+  void IsTrantorLaying(bool state);
 };
