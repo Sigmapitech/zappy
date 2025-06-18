c62ef3881ce5a6717928006a5771b1f3b716d54c
Ciznia <gabriel.hosquet@epitech.eu>
2025-06-18 13:49:56 +0200

    Add a printf like function to write on client

--- server/client.h
+++ server/client.h
@@ -14,5 +14,8 @@ void write_client(server_t *srv, uint32_t idx);
 void read_client(server_t *srv, uint32_t idx);
 
 void append_to_output(server_t *srv, client_state_t *client, const char *msg);
+[[gnu::format(printf, 3, 4)]]
+void vappend_to_output(
+    server_t *srv, client_state_t *client, const char *fmt, ...);
 
 #endif /* !CLIENT_H_ */
