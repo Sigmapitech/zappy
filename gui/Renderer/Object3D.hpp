9a1ea7bd8881e5778d3f7f60d3dfddef9c3ae5df
ElectronicIV <louis.persin@epitech.eu>
2025-06-19 10:48:00 +0200

    Refactor Object3D to be more readable

--- gui/Renderer/Object3D.hpp
+++ gui/Renderer/Object3D.hpp
@@ -3,14 +3,35 @@
 
 #include "Mesh.hpp"
 
-class Object3D {
+struct Object3D {
 private:
   std::unordered_map<std::string, size_t> _idMap;
 
-public:
-  glm::mat4 modelMatrix = glm::mat4(1.0);  // NOLINT
+  // parsing data
+  std::vector<glm::vec3> positions;
+  std::vector<glm::vec2> texCoords;
+  std::vector<glm::vec3> normals;
 
-  std::vector<std::unique_ptr<Mesh>> _meshArr;
+  std::string currentName = "default";
+  std::unique_ptr<std::vector<Vertex>> meshVertices = std::
+    make_unique<std::vector<Vertex>>();
+  std::unique_ptr<std::vector<unsigned int>> meshIndices = std::
+    make_unique<std::vector<unsigned int>>();
+
+  std::unordered_map<std::string, unsigned int> uniqueVertexMap;
+  unsigned int index = 0;
+
+  void HandleSubObject(std::istringstream &ss);
+  void HandleVertex(std::istringstream &ss);
+  void HandleVertexTexture(std::istringstream &ss);
+  void HandleVertexNormal(std::istringstream &ss);
+  void HandleFace(std::istringstream &ss);
+
+  void ParseLine(const std::string &prefix, std::istringstream &ss);
+
+public:
+  glm::mat4 modelMatrix = glm::mat4(1.0);       // NOLINT
+  std::vector<std::unique_ptr<Mesh>> _meshArr;  // NOLINT
 
   Object3D(const std::string &path);
   void
