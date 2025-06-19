60faa4d02d752bd74e9ff6081be79f18fa9893ce
ElectronicIV <louis.persin@epitech.eu>
2025-06-19 12:32:05 +0200

    Fix cs in asset_dir.h

--- gui/Renderer/asset_dir.hpp
+++ gui/Renderer/asset_dir.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-    #ifndef ASSET_DIR
+#ifndef ASSET_DIR
 /* Note: ASSET_DIR is set and controlled by the Makefile */
-        #define ASSET_DIR "."
-    #endif
+  #define ASSET_DIR "."
+#endif
