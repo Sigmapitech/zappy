ed9547457a302fd5fa66cd8f64d3912c7c7ace02
ElectronicIV <louis.persin@epitech.eu>
2025-06-19 10:48:00 +0200

    Refactor Object3D to be more readable

---
+++ gui/Renderer/Utils.hpp
@@ -0,0 +1,9 @@
+// DJB2 algorithm hashing function
+// constexpr used for compile-time
+constexpr unsigned int hash(const char *str)
+{
+  unsigned int h = 5381;  // Initial hash value
+  for (; *str != '\0'; str++)
+    h = (h * 33) ^ (unsigned char)*str;
+  return h;
+}
