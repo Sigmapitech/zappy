342fe19f152e75419135fda4ba8a524a3dd2a220
Valentin22r <valentin.roquejofre@epitech.eu>
2025-06-20 18:04:48 +0200

    Add Demeter to the main

--- gui/Zappy.hpp
+++ gui/Zappy.hpp
@@ -2,14 +2,16 @@
 
 #include <memory>
 
+#include "API/API.hpp"
 #include "Demeter/Demeter.hpp"
 
 struct Zappy {
 private:
   std::unique_ptr<Dem::Demeter> demeter;
+  std::shared_ptr<API> _api;
 
 public:
-  Zappy();
+  Zappy(std::shared_ptr<API> api);
   ~Zappy() = default;
 
   void Run();
