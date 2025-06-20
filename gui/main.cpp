95ed9607cfcda05a5f1a088cc36f47feb09bd8d7
Valentin22r <valentin.roquejofre@epitech.eu>
2025-06-20 18:04:48 +0200

    Add Demeter to the main

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,8 +1,11 @@
 #include <cstdlib>
 #include <exception>
 #include <iostream>
+#include <memory>
+#include <unistd.h>
 
 #include "ArgsParser.hpp"
+#include "Network/Network.hpp"
 #include "Zappy.hpp"
 #include "logging/Logger.hpp"
 
@@ -18,7 +21,9 @@ static constexpr const int EXIT_TEK_FAILURE = 84;
 [[gnu::weak]]
 int main(int argc, char *argv[])
 {
+  std::shared_ptr<API> data = std::make_shared<API>();
   Args params;
+
   Log::info << "GUI started.";
   if (!params.Parse(argc, argv))
     return EXIT_TEK_FAILURE;
@@ -28,23 +33,17 @@ int main(int argc, char *argv[])
   }
   std::cout << params;
 
-  Zappy zappy;
   try {
+    Network networkClass(params.GetPort(), params.GetHost(), data);
+    Log::info << "Network started.";
+    networkClass.RunNetwork();
+    Zappy zappy(data);
     zappy.Run();
+    networkClass.RequestStop();
   } catch (const std::exception &e) {
     Log::warn << "Runtime warn: " << e.what();
     return EXIT_TEK_FAILURE;
   }
 
-  /*
-  Network networkClass(params.GetPort(), params.GetHost());
-  Log::info << "Network started.";
-  try {
-    std::jthread network_thread(&Network::RunNetwork, &networkClass);
-  } catch (const std::runtime_error &e) {
-    Log::warn << "Runtime warn: " << e.what();
-    return EXIT_FAILURE;
-  }
-  */
   return EXIT_SUCCESS;
 }
