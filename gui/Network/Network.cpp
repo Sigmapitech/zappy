24eda644044303f616620cb45fb48e928b4205c9
ElectronicIV <louis.persin@epitech.eu>
2025-06-20 18:43:40 +0200

    Network clean up

--- gui/Network/Network.cpp
+++ gui/Network/Network.cpp
@@ -1,39 +1,64 @@
 #include "Network.hpp"
 #include "logging/Logger.hpp"
 
-#include <arpa/inet.h>
-#include <array>
-#include <memory>
 #include <netinet/in.h>
-#include <poll.h>
-#include <stdexcept>
-#include <string>
-#include <sys/poll.h>
-#include <sys/socket.h>
-#include <unistd.h>
 
+#include <array>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
 
 Network::Network(int port, std::string hostname, std::shared_ptr<API> &data)
-  : _port(port), _hostname(std::move(hostname))
+  : _port(port), _hostname(std::move(hostname)), _api(data)
 {
-  pipe(pipefd.data());
-
-  if (!data)
-    throw std::
-      runtime_error("Error: api, Function: Network, File: Network.cpp");
-  _api = data;
+  // Create server socket
   _fdServer = socket(AF_INET, SOCK_STREAM, 0);
-  if (_fdServer < 0) {
+  if (_fdServer == -1)
     throw std::
       runtime_error("Error: socket, Function: Network, File: Network.cpp");
-  }
+
+  // Set socket options
+  memset(&serverAddr, 0, sizeof(serverAddr));
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(_port);
+  socklen_t sockLen = sizeof(serverAddr);
+  // Convert hostname to IP address
+  if (inet_pton(AF_INET, _hostname.c_str(), &serverAddr.sin_addr) != 1)
+    throw std::runtime_error(
+      "Error: inet_pton failed. Function: RunNetwork, File: Network.cpp");
+
+  // Bind the socket to the address and port
+  if (connect(_fdServer, (const sockaddr *)(&serverAddr), sockLen) == -1)
+    throw std::runtime_error(
+      "Error: connect failed. Function: RunNetwork, File: Network.cpp");
+
+  // Initialize poll structures
+  _pollInFd[0].fd = _fdServer;
+  _pollInFd[0].events = POLLIN;
+  _pollInFd[0].revents = 0;
+  _pollInFd[1].fd = _pipeFdExit[0];
+  _pollInFd[1].events = POLLIN;
+  _pollInFd[1].revents = 0;
+
+  // Create pipe for thread exit
+  if (pipe(_pipeFdExit.data()) == -1)
+    throw std::runtime_error(
+      "Error: pipe failed. Function: Network, File: Network.cpp");
+
+  // Initialize pollOutFd for sending messages
+  _pollOutFd[0].events = POLLOUT;
+  _pollOutFd[0].fd = _fdServer;
+  _pollOutFd[0].revents = 0;
 }
 
 Network::~Network()
 {
-  close(pipefd[0]);
-  close(pipefd[1]);
+  shutdown(_fdServer, SHUT_RDWR);  // shut both end read and write
+  read(_fdServer, nullptr, 0);     // clear buffer
+  close(_fdServer);                // close socket
+
+  close(_pipeFdExit[0]);
+  close(_pipeFdExit[1]);
 }
 
 void Network::RunNetwork()
@@ -43,83 +68,50 @@ void Network::RunNetwork()
 
 void Network::RunNetworkInternal()
 {
-  sockaddr_in serverAddr{};
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(_port);
-  socklen_t sockLen = sizeof(serverAddr);
-
-  if (inet_pton(AF_INET, _hostname.c_str(), &serverAddr.sin_addr) != 1)
-    throw std::runtime_error(
-      "Error: inet_pton failed. Function: RunNetwork, File: Network.cpp");
-
-  if (
-    connect(
-      _fdServer, reinterpret_cast<const sockaddr *>(&serverAddr), sockLen)
-    < 0) {
-    throw std::runtime_error(
-      "Error: connect failed. Function: RunNetwork, File: Network.cpp");
-  }
-
-  bool end = false;
-  std::array<struct pollfd, 2> pollFd;
-  pollFd[0].fd = _fdServer;
-  pollFd[0].events = POLLIN | POLLOUT;
-  pollFd[0].revents = 0;
-  pollFd[1].fd = pipefd[0];
-  pollFd[1].events = POLLIN;
-  pollFd[1].revents = 0;
-
-  if (poll(pollFd.data(), 1, -1) == -1)
+  if (poll(_pollInFd.data(), _pollInFd.size(), -1) == -1)
     throw std::runtime_error(
       "Error: poll failed. Function: RunNetwork, File: Network.cpp");
 
-  if (pollFd[0].revents & POLLIN)
+  if (_pollInFd[0].revents & POLLIN)
     Log::info << "Message received: " << Log::cleanString(ReceiveMessage());
 
-  if (pollFd[0].revents & POLLOUT) {
-    std::string msg = "GRAPHIC\n";
-    SendMessage(msg);
-  }
+  SendMessage("GRAPHIC\n");
 
   int i = 0;
-  while (!end) {
-    if (poll(pollFd.data(), 1, -1) == -1)
+  bool isRunning = false;
+  while (!isRunning) {
+    if (poll(_pollInFd.data(), 1, -1) == -1)
       throw std::runtime_error(
         "Error: poll failed. Function: RunNetwork, File: Network.cpp");
 
     std::cerr << "Polling..." + std::to_string(i++) + "\n";
 
-    if (pollFd[1].revents & POLLIN) {
+    if (_pollInFd[1].revents & POLLIN) {
       std::array<char, 1> buffer;
-      if (read(pipefd[0], buffer.data(), buffer.size()) == -1)
+      if (read(_pipeFdExit[0], buffer.data(), buffer.size()) == -1)
         throw std::runtime_error(
           "Error: read failed. Function: RunNetwork, File: Network.cpp");
-      end = true;
+      isRunning = true;
       Log::info << "End of network thread requested.";
     }
 
-    if (pollFd[0].revents & POLLIN) {
+    if (_pollInFd[0].revents & POLLIN) {
       std::string message = ReceiveMessage();
       _api->ParseManageCommande(message);
     }
-    if (pollFd[0].revents & POLLOUT) {
-      for (std::string command: _api->GetCommand())
-        SendMessage(command);
-      _api->ClearCommand();
-    }
-
-    for (auto &fd: pollFd)
-      fd.revents = 0;  // Reset revents for the next poll
   }
-  close(_fdServer);
 }
 
-void Network::SendMessage(std::string &msg) const
+void Network::SendMessage(const std::string &msg)
 {
-  Log::info << "Message sent : " << Log::cleanString(msg);
+  if (poll(_pollOutFd.data(), _pollOutFd.size(), -1) == -1)
+    throw std::runtime_error(
+      "Error: poll failed, Function: SendMessage, File: Network.cpp");
+  // skip the POLLOUT check, there is only one fd in _pollOutFd
   if (send(_fdServer, msg.c_str(), msg.size(), 0) == -1)
     throw std::
       runtime_error("Error: send, Function: SendMessage, File: Network.cpp");
+  Log::info << "Message sent : " << Log::cleanString(msg);
 }
 
 std::string Network::ReceiveMessage() const
@@ -150,5 +142,5 @@ void Network::RequestStop()
 {
   std::cerr << "Requesting stop of network thread.\n";
   _networkThread.request_stop();
-  write(pipefd[1], "x", 1);
+  write(_pipeFdExit[1], "x", 1);
 }
