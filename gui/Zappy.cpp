8dbad49ce5860ecf6ad022aeba127f20b3883ef6
Valentin22r <valentin.roquejofre@epitech.eu>
2025-06-25 17:52:13 +0200

    Add all item on the map

--- gui/Zappy.cpp
+++ gui/Zappy.cpp
@@ -1,10 +1,11 @@
+#include <glm/trigonometric.hpp>
 #include <memory>
 
 #include "Demeter/Renderer/SDL2.hpp"
 #include "Entities/E_CameraControler.hpp"
 #include "Entities/E_Coms.hpp"
-#include "Entities/E_Default.hpp"
 #include "Entities/E_Light.hpp"
+#include "Entities/E_Mother.hpp"
 #include "Zappy.hpp"
 
 Zappy::Zappy(Args &params)
@@ -15,8 +16,9 @@ Zappy::Zappy(Args &params)
   // Register entities
   demeter->AddEntity(std::make_shared<E_Coms>(params));
   demeter->AddEntity(std::make_shared<E_Light>());
-  demeter->AddEntity(std::make_shared<E_Default>(*demeter));
+
   demeter->AddEntity(std::make_shared<E_CameraControler>(*demeter));
+  demeter->AddEntity(std::make_shared<E_Mother>(*demeter));
 
   // camera
   demeter->camera.position = glm::vec3(0.0, 1.0, 200.0);
